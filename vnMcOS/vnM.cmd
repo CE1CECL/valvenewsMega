d8146ad8f51666bf6035103c8befd368c0d21011
ChrisEric1 CECL <christopherericlentocha@gmail.com>
2022-06-07 17:20:00 -0400

    Update vnM.cmd

--- vnMcOS/vnM.cmd
+++ vnMcOS/vnM.cmd
@@ -1,3 +1,10 @@
+@echo off
+MKDIR C:\vnMcOS_td
+MKDIR C:\vnMcOS_td\
+CD /D C:\vnMcOS_td
+CD /D C:\vnMcOS_td\
+CD C:\vnMcOS_td
+CD C:\vnMcOS_td\
 :vnM
 echo Still Working as of %DATE% %TIME% 
 ping 127.0.0.1 > nul
