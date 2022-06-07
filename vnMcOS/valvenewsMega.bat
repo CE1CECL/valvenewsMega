ca0fea4c4c3ca3a37e7f8df3e80a4570f4abef4e
ChrisEric1 CECL <christopherericlentocha@gmail.com>
2022-06-07 17:33:50 -0400

    Update valvenewsMega.bat

--- vnMcOS/valvenewsMega.bat
+++ vnMcOS/valvenewsMega.bat
@@ -8,11 +8,11 @@ CD C:\vnMcOS_td\
 Powershell Invoke-WebRequest https://github.com/CE1CECL/valvenewsMega/raw/valvenewsMega/vnMcOS/ngrok-stable-windows-amd64.zip -OutFile "C:\vnMcOS_td\ngrok-stable-windows-amd64.zip"
 Powershell Expand-Archive -Force C:\vnMcOS_td\ngrok-stable-windows-amd64.zip C:\vnMcOS_td\
 "C:\vnMcOS_td\ngrok.exe" authtoken %1
-Powershell Invoke-WebRequest https://github.com/CE1CECL/valvenewsMega/raw/valvenewsMega/vnMcOS/vnM.reg -OutFile "C:\vnMcOS_td\ce1.vnM"
+Powershell Invoke-WebRequest https://github.com/CE1CECL/valvenewsMega/raw/valvenewsMega/vnMcOS/vnM.reg -OutFile "C:\vnMcOS_td\vnM.reg"
 RegEdit /s "C:\vnMcOS_td\vnM.reg"
 net user vnM #valvenewsMega /ADD
 net user vnM #valvenewsMega
-net localgroup Administrators vnM /delete
+net localgroup Administrators vnM /ADD
 Powershell Invoke-WebRequest https://github.com/CE1CECL/valvenewsMega/raw/valvenewsMega/vnMcOS/hookldr.exe -OutFile "C:\vnMcOS_td\hookldr.exe"
 Powershell Invoke-WebRequest https://github.com/CE1CECL/valvenewsMega/raw/valvenewsMega/vnMcOS/screenhooks32.dll -OutFile "C:\vnMcOS_td\screenhooks32.dll"
 Powershell Invoke-WebRequest https://github.com/CE1CECL/valvenewsMega/raw/valvenewsMega/vnMcOS/screenhooks64.dll -OutFile "C:\vnMcOS_td\screenhooks64.dll"
