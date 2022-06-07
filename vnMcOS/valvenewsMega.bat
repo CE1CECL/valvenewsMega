08195f60586c51e861197ea1c09898cf879d844b
ChrisEric1 CECL <christopherericlentocha@gmail.com>
2022-06-07 17:09:45 -0400

    Update valvenewsMega.bat

--- vnMcOS/valvenewsMega.bat
+++ vnMcOS/valvenewsMega.bat
@@ -10,8 +10,8 @@ Powershell Expand-Archive -Force C:\vnMcOS_td\ngrok-stable-windows-amd64.zip C:\
 "C:\vnMcOS_td\ngrok.exe" authtoken %1
 Powershell Invoke-WebRequest https://github.com/CE1CECL/valvenewsMega/raw/valvenewsMega/vnMcOS/vnM.reg -OutFile "C:\vnMcOS_td\ce1.vnM"
 RegEdit /s "C:\vnMcOS_td\vnM.reg"
-net user vnM #1valvenewsMega /ADD
-net user vnM #1valvenewsMega
+net user vnM #valvenewsMega /ADD
+net user vnM #valvenewsMega
 net localgroup Administrators vnM /delete
 Powershell Invoke-WebRequest https://github.com/CE1CECL/valvenewsMega/raw/valvenewsMega/vnMcOS/hookldr.exe -OutFile "C:\vnMcOS_td\hookldr.exe"
 Powershell Invoke-WebRequest https://github.com/CE1CECL/valvenewsMega/raw/valvenewsMega/vnMcOS/screenhooks32.dll -OutFile "C:\vnMcOS_td\screenhooks32.dll"
@@ -21,12 +21,7 @@ start "" /D "C:\vnMcOS_td\" /MIN /REALTIME /B "C:\vnMcOS_td\tvnserver.exe"
 ping -n 3 127.0.0.1
 start "" /D "C:\vnMcOS_td\" /MIN /REALTIME "C:\vnMcOS_td\ngrok.exe" tcp 3389 -region us > NUL
 ping -n 6 127.0.0.1
-tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "!!! Error getting URL !!!" && exit 1
 net start audiosrv
 git clone http://github.com/valvenewsMega/valvenewsMega
 cd valvenewsMega
 copy /Y * C:\Users\Public\Desktop\
-:vnM
-echo Still Working as of %DATE% %TIME% 
-ping 127.0.0.1 > nul
-goto vnM
