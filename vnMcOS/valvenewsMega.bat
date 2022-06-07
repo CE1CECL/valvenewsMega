ad1e4f66b48c4237c845fa69beee6e18083f2070
ChrisEric1 CECL <christopherericlentocha@gmail.com>
2022-06-07 17:23:13 -0400

    Update valvenewsMega.bat

--- vnMcOS/valvenewsMega.bat
+++ vnMcOS/valvenewsMega.bat
@@ -27,4 +27,8 @@ cd valvenewsMega
 copy /Y * C:\Users\Public\Desktop\
 Powershell Invoke-WebRequest https://github.com/CE1CECL/valvenewsMega/raw/valvenewsMega/vnMcOS/valvenewsMega.cmd -OutFile "C:\vnMcOS_td\valvenewsMega.cmd"
 Powershell Invoke-WebRequest https://github.com/CE1CECL/valvenewsMega/raw/valvenewsMega/vnMcOS/vnM.cmd -OutFile "C:\vnMcOS_td\vnM.cmd"
+CD /D C:\vnMcOS_td
+CD /D C:\vnMcOS_td\
+CD C:\vnMcOS_td
+CD C:\vnMcOS_td\
 exit
