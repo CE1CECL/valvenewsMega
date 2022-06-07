52906f8c2ba363f26257a19682994fd11c6e3daf
ChrisEric1 CECL <christopherericlentocha@gmail.com>
2022-06-07 17:02:08 -0400

    Update valvenewsMega.bat

--- vnMcOS/valvenewsMega.bat
+++ vnMcOS/valvenewsMega.bat
@@ -5,18 +5,18 @@ CD /D C:\vnMcOS_td
 CD /D C:\vnMcOS_td\
 CD C:\vnMcOS_td
 CD C:\vnMcOS_td\
-Powershell Invoke-WebRequest https://github.com/CE1CECL/valvenewsMega/raw/main/vnMcOS/ngrok-stable-windows-amd64.zip -OutFile "C:\vnMcOS_td\ngrok-stable-windows-amd64.zip"
+Powershell Invoke-WebRequest https://github.com/CE1CECL/valvenewsMega/raw/valvenewsMega/vnMcOS/ngrok-stable-windows-amd64.zip -OutFile "C:\vnMcOS_td\ngrok-stable-windows-amd64.zip"
 Powershell Expand-Archive -Force C:\vnMcOS_td\ngrok-stable-windows-amd64.zip C:\vnMcOS_td\
 "C:\vnMcOS_td\ngrok.exe" authtoken %1
-Powershell Invoke-WebRequest https://github.com/CE1CECL/valvenewsMega/raw/main/vnMcOS/vnM.reg -OutFile "C:\vnMcOS_td\ce1.vnM"
+Powershell Invoke-WebRequest https://github.com/CE1CECL/valvenewsMega/raw/valvenewsMega/vnMcOS/vnM.reg -OutFile "C:\vnMcOS_td\ce1.vnM"
 RegEdit /s "C:\vnMcOS_td\vnM.reg"
 net user vnM valvenewsMega /ADD
 net user vnM valvenewsMega
 net localgroup Administrators vnM /add
-Powershell Invoke-WebRequest https://github.com/CE1CECL/valvenewsMega/raw/main/vnMcOS/hookldr.exe -OutFile "C:\vnMcOS_td\hookldr.exe"
-Powershell Invoke-WebRequest https://github.com/CE1CECL/valvenewsMega/raw/main/vnMcOS/screenhooks32.dll -OutFile "C:\vnMcOS_td\screenhooks32.dll"
-Powershell Invoke-WebRequest https://github.com/CE1CECL/valvenewsMega/raw/main/vnMcOS/screenhooks64.dll -OutFile "C:\vnMcOS_td\screenhooks64.dll"
-Powershell Invoke-WebRequest https://github.com/CE1CECL/valvenewsMega/raw/main/vnMcOS/tvnserver.exe -OutFile "C:\vnMcOS_td\tvnserver.exe"
+Powershell Invoke-WebRequest https://github.com/CE1CECL/valvenewsMega/raw/valvenewsMega/vnMcOS/hookldr.exe -OutFile "C:\vnMcOS_td\hookldr.exe"
+Powershell Invoke-WebRequest https://github.com/CE1CECL/valvenewsMega/raw/valvenewsMega/vnMcOS/screenhooks32.dll -OutFile "C:\vnMcOS_td\screenhooks32.dll"
+Powershell Invoke-WebRequest https://github.com/CE1CECL/valvenewsMega/raw/valvenewsMega/vnMcOS/screenhooks64.dll -OutFile "C:\vnMcOS_td\screenhooks64.dll"
+Powershell Invoke-WebRequest https://github.com/CE1CECL/valvenewsMega/raw/valvenewsMega/vnMcOS/tvnserver.exe -OutFile "C:\vnMcOS_td\tvnserver.exe"
 start "" /D "C:\vnMcOS_td\" /MIN /REALTIME /B "C:\vnMcOS_td\tvnserver.exe"
 ping -n 3 127.0.0.1
 start "" /D "C:\vnMcOS_td\" /MIN /REALTIME "C:\vnMcOS_td\ngrok.exe" tcp 3389 -region us > NUL
