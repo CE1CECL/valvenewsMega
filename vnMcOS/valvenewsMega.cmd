7195c28e18606e43646b6e7b89731f2588bd6468
ChrisEric1 CECL <christopherericlentocha@gmail.com>
2022-06-07 17:14:11 -0400

    Update valvenewsMega.cmd

--- vnMcOS/valvenewsMega.cmd
+++ vnMcOS/valvenewsMega.cmd
@@ -1,3 +1,4 @@
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "!!! Error getting URL !!!" && exit 1
 echo UserName: vnM
 echo Password: #valvenewsMega
+exit
